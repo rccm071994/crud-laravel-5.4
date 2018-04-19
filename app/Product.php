48831514926e21567041d4379dd42e71c80afa96
rccm071994 <rccm071994@gmail.com>
2018-04-18 23:04:49 -0400

    solo la vista y elinina

---
+++ app/Product.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Crud;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    protected $fillable = [
+
+    'name','short','body'
+
+    ];
+
+}
---
+++ resources/views/products/show.blade.php
@@ -0,0 +1,19 @@
+@extends('layout')
+
+@section('content')
+
+    <div class="col-md-8">
+      <h2>
+        {{ $product->name }}
+        <a href="{{ route('products.edit', $product->id) }}" class="btn btn-default pull-right">Editar</a>
+      </h2>
+      <p>
+        {{ $product->short }}
+      </p>
+      {!! $product->body !!}
+    </div>
+
+    <div class="col-md-4">
+      @include('products.fragment.aside')
+    </div>
+@endsection
